c1b42a0270bed9788b9fdbd403d66b1acba0178b
pilgwon <rlavlfrnjs12@gmail.com>
2020-04-29 17:00:28 +0900

    ✨Support SPM

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DPToastView",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "DPToastView",
+            targets: ["DPToastView"]
+        ),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "DPToastView",
+            dependencies: [],
+            path: "",
+            exclude: []
+        ),
+    ]
+)
